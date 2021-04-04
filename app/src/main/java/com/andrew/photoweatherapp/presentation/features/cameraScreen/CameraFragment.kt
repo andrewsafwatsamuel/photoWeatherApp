012b728ee0f565e1b64c370abc92db1a5659e901
Andrew <AndrewSafwatSamuel@gmail.com>
2021-04-04 12:18:01 +0200

    added viewBinding

--- app/src/main/java/com/andrew/photoweatherapp/presentation/features/cameraScreen/CameraFragment.kt
+++ app/src/main/java/com/andrew/photoweatherapp/presentation/features/cameraScreen/CameraFragment.kt
@@ -2,15 +2,22 @@ package com.andrew.photoweatherapp.presentation.features.cameraScreen
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.andrew.photoweatherapp.R
+import com.andrew.photoweatherapp.databinding.FragmentCameraBinding
 
 class CameraFragment:Fragment() {
+
+    private lateinit var binding : FragmentCameraBinding
+
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
-    )=inflater.inflate(R.layout.fragment_camera,container,false)
+    ): View {
+        binding = FragmentCameraBinding.inflate(inflater)
+        return binding.root
+    }
 
 }
