39e9bf5cecff2aaf7141361caf817d1c2f7cd799
Andrew <AndrewSafwatSamuel@gmail.com>
2021-04-05 22:56:43 +0200

    ShareFragment drawn large photo

--- app/src/main/java/com/andrew/photoweatherapp/ShareFragment.kt
+++ app/src/main/java/com/andrew/photoweatherapp/ShareFragment.kt
@@ -1,15 +1,34 @@
 package com.andrew.photoweatherapp
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.andrew.photoweatherapp.databinding.FragmentShareBinding
 
 class ShareFragment : Fragment() {
 
+    private val path by lazy { ShareFragmentArgs.fromBundle(requireArguments()).uri }
+    private lateinit var binding: FragmentShareBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View = inflater.inflate(R.layout.fragment_share, container, false)
+    ): View {
+        binding = FragmentShareBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (path.isBlank()) {
+            binding.fullPhotoImageView.scaleType = ImageView.ScaleType.CENTER
+            binding.fullPhotoImageView.setImageResource(R.drawable.ic_picture)
+        } else {
+            binding.fullPhotoImageView.setImageURI(Uri.parse(path))
+        }
+
+    }
 }
