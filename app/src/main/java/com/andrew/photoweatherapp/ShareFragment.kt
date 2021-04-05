609f534ac3a2d632a2958f3db4294d762687bcd6
Andrew <AndrewSafwatSamuel@gmail.com>
2021-04-06 01:38:32 +0200

    implemented Share functions

--- app/src/main/java/com/andrew/photoweatherapp/ShareFragment.kt
+++ app/src/main/java/com/andrew/photoweatherapp/ShareFragment.kt
@@ -2,15 +2,14 @@ package com.andrew.photoweatherapp
 
 import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import com.andrew.photoweatherapp.databinding.FragmentShareBinding
 
 class ShareFragment : Fragment() {
-
     private val path by lazy { ShareFragmentArgs.fromBundle(requireArguments()).uri }
     private lateinit var binding: FragmentShareBinding
     override fun onCreateView(
@@ -29,6 +28,6 @@ class ShareFragment : Fragment() {
         } else {
             binding.fullPhotoImageView.setImageURI(Uri.parse(path))
         }
-
     }
+
 }
---
+++ app/src/main/java/com/andrew/photoweatherapp/presentation/ImageShareHelper.kt
@@ -0,0 +1,44 @@
+package com.andrew.photoweatherapp.presentation
+
+import android.content.Context
+import android.content.Intent
+import android.content.Intent.ACTION_VIEW
+import android.net.Uri
+import android.widget.Toast
+import androidx.core.content.FileProvider
+import java.io.File
+
+const val TWITTER_PACKAGE="com.twitter.android"
+const val FACEBOOK_PACKAGE="com.facebook.katana"
+
+fun shareImage(context: Context, imagePath: String, packageId: String) =
+    if (context.packageManager.getLaunchIntentForPackage(packageId) == null) {
+        Toast.makeText(context, "Please first install the app", Toast.LENGTH_SHORT).show()
+        context.startPlayStore(packageId)
+    } else {
+        context.composeImageUri(imagePath).let { context.startIntent(packageId, it) }
+    }
+
+private fun Context.startIntent(packageId: String, uri: Uri) = try {
+    startShareIntent(packageId, uri)
+} catch (ex: Exception) {
+    Toast.makeText(this, "Please first install the app", Toast.LENGTH_SHORT).show()
+    startPlayStore(packageId)
+}
+
+private fun startShareIntent(
+    packageId: String,
+    uri: Uri
+) = Intent(Intent.ACTION_SEND).apply {
+    setPackage(packageId)
+    type = "image/*"
+    putExtra(Intent.EXTRA_STREAM, uri)
+}.let { Intent.createChooser(it, "Share Image") }
+
+private fun Context.startPlayStore(packageId: String) = Intent(
+    ACTION_VIEW,
+    Uri.parse("https://play.google.com/store/apps/details?id=$packageId")
+).let { startActivity(it) }
+
+private fun Context.composeImageUri(imagePath: String): Uri = "${applicationContext.packageName}.provider"
+    .let { FileProvider.getUriForFile(this, it, File(imagePath)) }
