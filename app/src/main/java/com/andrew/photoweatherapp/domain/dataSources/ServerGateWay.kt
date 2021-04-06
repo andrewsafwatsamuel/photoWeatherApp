1d2a1173c1cc1ff4fd1138162b9e49f5c353aa49
Andrew <AndrewSafwatSamuel@gmail.com>
2021-04-06 05:52:55 +0200

    fixed https issue for api 21

--- app/src/main/java/com/andrew/photoweatherapp/domain/dataSources/ServerGateWay.kt
+++ app/src/main/java/com/andrew/photoweatherapp/domain/dataSources/ServerGateWay.kt
@@ -1,5 +1,6 @@
 package com.andrew.photoweatherapp.domain.dataSources
 
+import android.os.Build
 import com.andrew.photoweatherapp.entities.WeatherData
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
@@ -10,6 +11,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 private const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+private const val BASE_URL_21 = "http://api.openweathermap.org/data/2.5/"
 private const val APP_ID = "86a9abf0018f9fe698c39241d29ee0ab"
 
 private fun request(chain: Interceptor.Chain): okhttp3.Response = with(chain.request()) {
@@ -29,11 +31,14 @@ private val httpClient by lazy {
 
 private val retrofitInstance by lazy {
     Retrofit.Builder()
-        .baseUrl(BASE_URL)
+        .baseUrl(setBaseUrl())
         .addConverterFactory(GsonConverterFactory.create())
         .client(httpClient)
         .build()
 }
+
+private fun setBaseUrl() =
+    if (Build.VERSION.SDK_INT == Build.VERSION_CODES.LOLLIPOP) BASE_URL_21 else BASE_URL
 
 val apiClient: ApiClient by lazy { retrofitInstance.create(ApiClient::class.java) }
 
